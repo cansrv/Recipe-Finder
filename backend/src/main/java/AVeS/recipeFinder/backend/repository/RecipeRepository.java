524a50167f2c0dc6a35739637ddbc95082633e9f
haruncansurav <can.surav@ug.bilkent.edu.tr>
2023-05-27 17:27:23 +0300

    Implemented Recipe Service and its required repository methods

--- backend/src/main/java/AVeS/recipeFinder/backend/repository/RecipeRepository.java
+++ backend/src/main/java/AVeS/recipeFinder/backend/repository/RecipeRepository.java
@@ -3,7 +3,22 @@ package AVeS.recipeFinder.backend.repository;
 import AVeS.recipeFinder.backend.entity.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    public Recipe findRecipeByName(String name);
+
+    public List<Recipe> findRecipesByNameLike(String name);
+
+    public List<Recipe> findRecipesByCategoryContaining(String category);
+
+    public List<Recipe> findRecipesByTagsContains(String tag);
+
+    public List<Recipe> findRecipesByTagsIn(List<String> tags);
+
+    public List<Recipe> findRecipesByDurationLessThanEqual(int duration);
+
+
+
 }
