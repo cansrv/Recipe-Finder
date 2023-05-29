eccd4eea05d97f9983802cc9227bf3f2f7260e13
haruncansurav <can.surav@ug.bilkent.edu.tr>
2023-05-29 21:00:00 +0300

    Tests now use Assertions library

--- backend/src/test/java/AVeS/recipeFinder/backend/InventoryTest.java
+++ backend/src/test/java/AVeS/recipeFinder/backend/InventoryTest.java
@@ -1,33 +1,29 @@
 package AVeS.recipeFinder.backend;
 
 
-import AVeS.recipeFinder.backend.DTO.CustomerDTO;
-import AVeS.recipeFinder.backend.DTO.InventoryDTO;
+import AVeS.recipeFinder.backend.entity.dto.InventoryDTO;
 import AVeS.recipeFinder.backend.entity.model.Customer;
 import AVeS.recipeFinder.backend.entity.model.Ingredient;
 import AVeS.recipeFinder.backend.entity.model.Inventory;
-import AVeS.recipeFinder.backend.repository.ComponentRepository;
 import AVeS.recipeFinder.backend.repository.CustomerRepository;
 import AVeS.recipeFinder.backend.repository.IngredientRepository;
 import AVeS.recipeFinder.backend.repository.InventoryRepository;
 import AVeS.recipeFinder.backend.service.CustomerService;
 import AVeS.recipeFinder.backend.service.InventoryService;
-import AVeS.recipeFinder.backend.service.RecipeService;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
 @SpringBootTest
 public class InventoryTest {
     @Resource
     private IngredientRepository ingredientRepository;
-    @Resource
-    private ComponentRepository componentRepository;
-    @Resource
-    private RecipeService recipeService;
-
     @Resource
     private CustomerService customerService;
 
@@ -36,13 +32,8 @@ public class InventoryTest {
 
     @Resource
     private CustomerRepository customerRepository;
-
-
     @Resource
     private InventoryRepository inventoryRepository;
-
-
-
     @Test
     void testCRUD() {
 
@@ -65,9 +56,6 @@ public class InventoryTest {
         Long ingredientId = ingredientRepository.findAll().get(0).getId();
         Long customerId = customerRepository.findAll().get(0).getId();
 
-        System.out.println(customerId);
-        System.out.println(ingredientId);
-
         InventoryDTO newInventory = InventoryDTO.builder()
                 .ingredientId(ingredientId)
                 .customerId(customerId)
@@ -83,22 +71,21 @@ public class InventoryTest {
                 .build();
 
         inventoryService.addInventory(newInventory);
-        //inventoryRepository.save(inventory);
 
 
         List<Inventory> fridge = inventoryService.getCustomerFridge(1L);
-        System.out.println(fridge.get(0).getQuantity());
+        assertEquals(3.0, fridge.get(0).getQuantity());
         inventoryService.incrementInventoryQuantity(1L, 3);
         List<Inventory> fridge2 = inventoryService.getCustomerFridge(1L);
-        System.out.println(fridge2.get(0).getQuantity());
+        assertEquals(6.0, fridge2.get(0).getQuantity());
 
         inventoryService.decrementInventoryQuantity(1L, 2);
         List<Inventory> fridge3 = inventoryService.getCustomerFridge(1L);
-        System.out.println(fridge3.get(0).getQuantity());
+        assertEquals(4.0, fridge3.get(0).getQuantity());
 
-        System.out.println(inventoryService.searchInventoryByName("st", 1L).get(0).getIngredient().getName());
+        assertEquals("strawberry",inventoryService.searchInventoryByName("st", 1L).get(0).getIngredient().getName());
 
         inventoryService.deleteInventory(1L);
-        System.out.println(inventoryRepository.findInventoryById(1L));
+        assertNull(inventoryRepository.findInventoryById(1L));
     }
 }
